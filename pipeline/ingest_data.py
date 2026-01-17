9e4858fa786c2dc2d12f1ac574a7b1f8f1ff0c10
Ruslan Lyubimov <79132659+Lyubimov-Ruslan2@users.noreply.github.com>
2026-01-17 23:17:59 +0000

    finish docker

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 
 dtype = {
@@ -33,48 +34,88 @@ parse_dates = [
 
 
 
-
-
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host =  'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-
-
-    year = 2021
-    month = 1
-    
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
-    
-    
-
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+def run(
+    pg_user: str = "root",
+    pg_pass: str = "root",
+    pg_host: str = "localhost",
+    pg_port: int = 5432,
+    pg_db: str = "ny_taxi",
+    year: int = 2021,
+    month: int = 1,
+    target_table: str = "yellow_taxi_data",
+    chunksize: int = 100000,
+    prefix: str = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow",
+):
     url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-    
+    engine = create_engine(
+        f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    )
+
     df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=chunksize
-)
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
-            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
+            df_chunk.head(0).to_sql(
+                name=target_table, con=engine, if_exists="replace"
+            )
             first = False
-        df_chunk.to_sql(name=target_table, con=engine, if_exists='append')
-        
-        
-        
-        
-if __name__ == '__main__':
-    run()
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
+
+
+@click.command()
+@click.option("--pg-user", default="root", show_default=True, help="Postgres user")
+@click.option("--pg-pass", default="root", show_default=True, help="Postgres password")
+@click.option("--pg-host", default="localhost", show_default=True, help="Postgres host")
+@click.option("--pg-port", default=5432, show_default=True, help="Postgres port", type=int)
+@click.option("--pg-db", default="ny_taxi", show_default=True, help="Postgres database")
+@click.option("--year", default=2021, show_default=True, type=int, help="Dataset year")
+@click.option("--month", default=1, show_default=True, type=int, help="Dataset month")
+@click.option(
+    "--target-table",
+    default="yellow_taxi_data",
+    show_default=True,
+    help="Destination table name",
+)
+@click.option(
+    "--chunksize",
+    default=100000,
+    show_default=True,
+    type=int,
+    help="Number of rows per chunk",
+)
+@click.option(
+    "--prefix",
+    default="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow",
+    show_default=True,
+    help="Base URL prefix for the dataset",
+)
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize, prefix):
+    """Ingest NYC taxi data into Postgres."""
+
+    run(
+        pg_user=pg_user,
+        pg_pass=pg_pass,
+        pg_host=pg_host,
+        pg_port=pg_port,
+        pg_db=pg_db,
+        year=year,
+        month=month,
+        target_table=target_table,
+        chunksize=chunksize,
+        prefix=prefix,
+    )
+
+
+if __name__ == "__main__":
+    main()
     
 
 
